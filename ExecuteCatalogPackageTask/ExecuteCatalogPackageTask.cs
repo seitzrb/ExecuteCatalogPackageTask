cfbde7f3932e319ffa662a1a287db98a9f46c4a8
seitzrb <rseitz@larimer.org>
2018-06-21 13:27:23 -0600

    End of Chapter 5 save

--- ExecuteCatalogPackageTask/ExecuteCatalogPackageTask.cs
+++ ExecuteCatalogPackageTask/ExecuteCatalogPackageTask.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.SqlServer.Dts.Runtime;
 
 // 7d99f73f76fa51df
-public class ExecuteCatalogPackageTask
+[DtsTask(DisplayName = "xecuteCatalogPackageTask", Description = "A Task to execute packages stored in the SSIS Catelog")]
+public class ExecuteCatalogPackageTask : Task
 {
-
+    private string _serverName;
+    private string _packageCatalog = "SSISDB";
+    private string _packageFolder;
+    private string _packageProject;
+    public string ServerName
+    {
+        get { return _serverName; }
+        set { _serverName = value; }
+    }
+    public override void InitializeTask(Connections connections, VariableDispenser variableDispenser, IDTSInfoEvents events, IDTSLogging log, EventInfos eventInfos, LogEntryInfos logEntryInfos, ObjectReferenceTracker refTracker)
+    {
+        
+    }
+    public override DTSExecResult Validate(Connections connections, VariableDispenser variableDispenser, IDTSComponentEvents componentEvents, IDTSLogging log)
+    {
+        return DTSExecResult.Success;
+    }
+    public override DTSExecResult Execute(Connections connections, VariableDispenser variableDispenser, IDTSComponentEvents componentEvents, IDTSLogging log, object transaction)
+    {
+        return DTSExecResult.Success;
+    }
 }
 
